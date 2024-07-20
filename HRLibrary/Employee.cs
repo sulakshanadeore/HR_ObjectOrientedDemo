cf54fb5aa068912a98f079d03cd08e2e4226e5e7
deoresulakshana <deoresulakshana@gmail.com>
2024-07-20 09:48:15 +0530

    Add project files.

---
+++ HRLibrary/Employee.cs
@@ -0,0 +1,39 @@
+﻿namespace HRLibrary
+{
+    public class Employee
+    {
+        static int _empno;
+        string? _fname, _lname, _city;
+        long? _adharno;
+
+        static Employee()
+        {
+            _empno = 0;
+            Console.WriteLine("empno initialized....");
+        }
+        public int NewEmployee(string? fname,string? lname,string? city,long? adharno)
+        {
+            _empno++;
+
+
+            this._fname = fname;
+            this._lname = lname;
+            this._city = city;
+            this._adharno = adharno;    
+        return _empno;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine(this._fname);
+            Console.WriteLine(this._lname);
+            Console.WriteLine(this._city);
+            Console.WriteLine(this._adharno);
+
+        }
+
+
+
+
+    }
+}
---
+++ OOPsApp/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using HRLibrary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        
+
+
+
+        Employee employee = new Employee();
+        Console.WriteLine("Enter first name ");
+        string? firstname=Console.ReadLine();
+        Console.WriteLine("Enter LastName");
+        string? lastname=Console.ReadLine();
+        Console.WriteLine("Enter City");
+        string? city=Console.ReadLine();
+        Console.WriteLine("Enter Aadhar No");
+        long? adhar=Convert.ToInt64(Console.ReadLine());
+        int empno=employee.NewEmployee(firstname,lastname,city,adhar);
+        Console.WriteLine("Printing details... You entered the following");
+        employee.PrintData();
+        Console.WriteLine($"Generated EmployeeNumber={empno}");
+
+        Console.Read();
+    }
+}
